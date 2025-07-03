c94afa7214b6fdc907c19d1ec32eea19cd0a111d
Iben Youssef Wiem <wiemb55@gmail.com>
2025-07-03 17:49:44 +0100

    Delete-User-ApiTest

--- skill-management/src/main/java/com/example/skill_management/model/User.java
+++ skill-management/src/main/java/com/example/skill_management/model/User.java
@@ -41,7 +41,7 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Token> tokens;
 
     @PrePersist
--- skill-management/src/main/java/com/example/skill_management/token/Token.java
+++ skill-management/src/main/java/com/example/skill_management/token/Token.java
@@ -28,6 +28,6 @@ public class Token {
   public boolean expired;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id")
-  public User user;
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 }
